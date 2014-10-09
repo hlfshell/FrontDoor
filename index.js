c0853b6edaffd511108e020d08101ab64f035140
Keith Chester <kchester@gmail.com>
2014-10-09 18:53:41 -0500

    Extraneous :

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var http = require('http'),
 	twilioAccountSID = process.env.TWILIO_ACCOUNT_SID,
 	twilioAuthToken = process.env.TWILIO_AUTH_TOKEN,
 	doorPhoneNumber = process.env.DOOR_PHONE_NUMBER,
-	textOnEntry = process.env.TEXT_ON_ENTRY ? : JSON.stringify(process.env.TEXT_ON_ENTRY) : null
+	textOnEntry = process.env.TEXT_ON_ENTRY ? JSON.stringify(process.env.TEXT_ON_ENTRY) : null
 
 app.post('/frontDoor', twilio.webhook, function(req, res){
 	console.log(req.params)
